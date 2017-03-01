48492b808f713f554c77497e12784b57a7f6a758
Lewis Jang <lewis.aedes@gmail.com>
2017-03-01 14:10:07 +0800

    update some script

--- skill6.sikuli/skill6.py
+++ skill6.sikuli/skill6.py
@@ -1,17 +1,32 @@
 def click_min():
-    click(Pattern("1488167445870.png").similar(0.50).targetOffset(8,65))
+    hover("1488347106668.png")
+    keyDown(Key.CTRL)
+    click("1488347106668.png")
+    keyUp(Key.CTRL)
 
 def change_weapon():
     pass
 
 def start_skill():
-    type(Key.F11)
+    doubleClick(Pattern("1488348198034.png").targetOffset(119,-4))
     click_min()
     sleep(0.3)
 
+def level_up():
+    try:
+        if find("1488093197899.png"):
+            for i in findAll("1488093197899.png"):
+                doubleClick(i)
+                sleep(1)
+                click("1488093218212.png")
+                sleep(1)
+                click("1488093231068.png")
+    except:
+        pass
+
 for i in range(1000):
     if i % 6 == 0:
-        change_weapon()
+        level_up()
     start_skill()
 
     
